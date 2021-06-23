8a71c83668fdacd26bbd254229270b6fb9169ed7
dpilafian <dem@centerkey.com>
2021-06-23 04:16:39 -0700

    assertDeepStrictEqual() source

---
+++ assert-deep-strict-equal.ts
@@ -0,0 +1,20 @@
+// assert-deep-strict-equal ~ MIT License
+
+import { deepStrictEqual } from 'assert';
+
+const assertDeepStrictEqual = (actual: unknown, expected: unknown, done?: (e?: unknown) => void): void => {
+   const toPlainObj = (obj: unknown): unknown => JSON.parse(JSON.stringify(obj));
+   try {
+      deepStrictEqual(toPlainObj(actual), toPlainObj(expected));
+      if (done)
+         done();
+      }
+   catch(error) {
+      if (done)
+         done(error);
+      else
+         throw error;
+      }
+   };
+
+export { assertDeepStrictEqual };
