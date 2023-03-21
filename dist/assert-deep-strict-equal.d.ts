085dc22f569e84d1151e18b9d4d9cf1dfe278195
dpilafian <dem@centerkey.com>
2023-03-21 14:01:28 -0700

    Release v1.0.6

--- dist/assert-deep-strict-equal.d.ts
+++ dist/assert-deep-strict-equal.d.ts
@@ -1,4 +1,4 @@
-//! assert-deep-strict-equal v1.0.5 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
+//! assert-deep-strict-equal v1.0.6 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
 
 declare const assertDeepStrictEqual: (actual: unknown, expected: unknown, done?: ((e?: unknown) => void) | undefined) => void;
 export { assertDeepStrictEqual };
