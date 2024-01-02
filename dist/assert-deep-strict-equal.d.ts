e652c2338e18fcf60172c55527d7fa9abc671832
dpilafian <dem@centerkey.com>
2024-01-01 19:27:02 -0800

    Release v1.1.2

--- dist/assert-deep-strict-equal.d.ts
+++ dist/assert-deep-strict-equal.d.ts
@@ -1,4 +1,4 @@
-//! assert-deep-strict-equal v1.1.1 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
+//! assert-deep-strict-equal v1.1.2 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
 
 declare const assertDeepStrictEqual: (actual: unknown, expected: unknown, done?: ((e?: unknown) => void) | undefined) => void;
 export { assertDeepStrictEqual };
--- dist/assert-deep-strict-equal.js
+++ dist/assert-deep-strict-equal.js
@@ -1,4 +1,4 @@
-//! assert-deep-strict-equal v1.1.1 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
+//! assert-deep-strict-equal v1.1.2 ~~ https://github.com/center-key/assert-deep-strict-equal ~~ MIT License
 
 import { deepStrictEqual } from 'assert';
 const assertDeepStrictEqual = (actual, expected, done) => {
